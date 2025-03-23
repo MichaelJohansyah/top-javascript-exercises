42e667c45348a2b62311c29df7efb8e9b9458482
Michael Johansyah <johansyahmichael@gmail.com>
2025-03-23 23:04:28 +0700

    Complete challenge 3, 4 and 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  }
 
+  let totalNum = 0;
+  if (num1 < num2) {
+    i = num1;
+    while (i <= num2) {
+      totalNum += i;
+      i++;
+    }
+  } else if (num1 > num2) {
+    i = num2;
+    while (i <= num1) {
+      totalNum += i;
+      i++;
+    }
+  } else {
+    return num1;
+  }
+  return totalNum;
 };
 
 // Do not edit below this line
