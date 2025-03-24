fd5f2118632ee00ff8f87531c3a230a361a766db
Michael Johansyah <johansyahmichael@gmail.com>
2025-03-24 08:54:53 +0700

    Complete challenge 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let celc = (degree - 32) * 5/9;
+  if (celc >= 0){
+    celc = Math.round(celc * 10) / 10;
+  }
+  else{
+    celc = Math.ceil(celc * 10) / 10;
+  }
+  return celc;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let fheit = degree * 9/5 + 32;
+  if (fheit >= 0){
+    fheit = Math.round(fheit * 10) / 10;
+  }
+  else{
+    fheit = Math.ceil(fheit * 10) / 10;
+  }  
+  return fheit;
 };
 
 // Do not edit below this line
