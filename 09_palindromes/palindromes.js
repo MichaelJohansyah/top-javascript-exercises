ab505c5db00dbc6e9df8a130b8b01e49770249c9
Michael Johansyah <johansyahmichael@gmail.com>
2025-04-29 10:09:33 +0700

    Complete palindrome and fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz1234567890';
 
+    const cleanStr = str
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+
+    const reversedStr = cleanStr.split('').reverse().join('');
+    return cleanStr === reversedStr;
 };
 
 // Do not edit below this line
