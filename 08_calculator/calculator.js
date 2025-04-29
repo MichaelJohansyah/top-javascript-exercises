04476ccc18ea3ccc48a4d4298ad7ca13ddbb6d15
Michael Johansyah <johansyahmichael@gmail.com>
2025-04-29 09:09:57 +0700

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,47 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (a = []) {
+    let total = 0;
+    a.forEach((num) => {
+        total += num;
+    });
+    return total;
 };
 
-const multiply = function() {
-
+const multiply = function (a = []) {
+  let total = 1;
+    a.forEach((num) => {
+        total *= num;
+    });
+    return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a <= 1) { 
+    return 1;
+  }
+  else{
+    return (a * factorial(a - 1));
+  }
+
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
